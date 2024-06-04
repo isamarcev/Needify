c67cb831ada7f16a61a901c8beb1c8a13c2ec9a6
Ihor Samartsev <isamarcev1994@gmail.com>
2024-06-04 11:01:32 +0300

    fixing process

--- contracts/TheOpenTimes/scripts/deployJobOffer.ts
+++ contracts/TheOpenTimes/scripts/deployJobOffer.ts
@@ -148,14 +148,6 @@ export async function run(provider: NetworkProvider) {
         forward_payload: beginCell().endCell()
     })).endCell()
 
-    let NativeDepositMsg = beginCell().store(storeDeposit({
-        $$type: "Deposit",
-        query_id: 1n,
-        amount: PublicCost,
-        destination: JOInitAddress,
-        response_destination: poster_wallet_contract.address,
-        forward_ton_amount: toNano(0.2),
-    })).endCell()
     let NativeTransferMsg = beginCell().store(storeTransfer({
         $$type: "Transfer",
         query_id: 1n,
@@ -167,45 +159,49 @@ export async function run(provider: NetworkProvider) {
         forward_payload: beginCell().endCell()
     })).endCell()
     let seqno = await poster_wallet_contract.getSeqno();
-    // await poster_wallet_contract.sendTransfer({
-    //     seqno,
-    //     secretKey: secretKeyPoster,
-    //     messages: [
-    //         internal({
-    //             to: JOInitAddress,
-    //             value: deployAmount * 2n,
-    //             bounce: false,
-    //             init: {
-    //                 code: JOInit.code,
-    //                 data: JOInit.data,
-    //             },
-    //             body: deploy_msg,
-    //         }),
-    //         internal({
-    //             to: poster_native_wallet.address,
-    //             value: deployAmount * 2n,
-    //             bounce: false,
-    //             // init: {
-    //             //     code: JOJN.init?.code,
-    //             //     data: JOJN.init?.data,
-    //             // },
-    //             body: NativeTransferMsg,
-    //         }),
-    //         internal({
-    //             to: poster_jetton_wallet.address,
-    //             value: deployAmount * 2n,
-    //             bounce: false,
-    //             // init: {
-    //             //     code: JOJW.init?.code,
-    //             //     data: JOJW.init?.data,
-    //             // },
-    //             body: TokenTransferMsg,
-    //         }),
-    //     ],})
+    await poster_wallet_contract.sendTransfer({
+        seqno,
+        secretKey: secretKeyPoster,
+        messages: [
+            internal({
+                to: JOInitAddress,
+                value: deployAmount * 2n,
+                bounce: false,
+                init: {
+                    code: JOInit.code,
+                    data: JOInit.data,
+                },
+                body: deploy_msg,
+            }),
+            internal({
+                to: poster_native_wallet.address,
+                value: deployAmount * 2n,
+                bounce: false,
+                // init: {
+                //     code: JOJN.init?.code,
+                //     data: JOJN.init?.data,
+                // },
+                body: NativeTransferMsg,
+            }),
+            internal({
+                to: poster_jetton_wallet.address,
+                value: deployAmount * 2n,
+                bounce: false,
+                // init: {
+                //     code: JOJW.init?.code,
+                //     data: JOJW.init?.data,
+                // },
+                body: TokenTransferMsg,
+            }),
+        ],})
 
 
-    let JOstate = await job_offer.getJobData()
-    log("Job offer state: ", JOstate)
+    // let JOstate = await job_offer.getJobData()
+    // log("Job offer state: ", JOstate)
+
+
+
+
 
     // // Revoke 
     // let msg_revoke = beginCell().store(storeRevoke({
