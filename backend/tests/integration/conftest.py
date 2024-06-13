d6196ed1ee88625d71454399a63efe4d73fbb3eb
Ihor Samartsev <isamarcev1994@gmail.com>
2024-06-13 23:24:39 +0300

    starting tests

--- backend/tests/integration/conftest.py
+++ backend/tests/integration/conftest.py
@@ -4,7 +4,10 @@ import pytest
 from pymongo import MongoClient
 
 from src.apps.users.dependencies import UserContainer
+from src.apps.utils.database import ThreadMongoSingleton
 from src.core.config import config
+from src.core.database import setup_database
+from src.core.dependencies import CoreContainer
 
 
 @pytest.fixture(scope="session")
@@ -23,6 +26,21 @@ def user_container():
     container.config.from_pydantic(settings=config)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(container.user_manager().setup_database())
+    # asyncio.run_coroutine_threadsafe(container.user_manager().setup_database())
+    yield container
+
+    # Drop test database
+    sync_mongo = MongoClient(config.MONGO_DB_URL)
+    sync_mongo.drop_database("TEST_DATABASE_NAME")
+
+
+@pytest.fixture(scope="session")
+def core_container():
+    container = CoreContainer()
+    config.MONGO_DB_NAME = "TEST_DATABASE_NAME"
+    loop = asyncio.get_event_loop()
+    async_mongo_test = ThreadMongoSingleton(config.MONGO_DB_URL, config.MONGO_DB_NAME)
+    loop.run_until_complete(setup_database(async_mongo_test))
     # asyncio.run_coroutine_threadsafe(container.user_manager().setup_database())
     yield container
 
