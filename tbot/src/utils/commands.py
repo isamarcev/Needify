6cfcf61ef0f5a8703587d8fb63d88852c1b65b59
Ihor Samartsev <isamarcev1994@gmail.com>
2024-06-16 23:24:24 +0300

    creating referral system. added user database management added logic for getting reward

--- tbot/src/utils/commands.py
+++ tbot/src/utils/commands.py
@@ -6,5 +6,7 @@ from aiogram.types import BotCommand
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(command="start", description="Go Go Go 🚀"),
+        BotCommand(command="get_referral", description="Get referral link 🤑"),
+        BotCommand(command="share_referral", description="Share referral link 📢"),
     ]
     await bot(SetMyCommands(commands=commands))
