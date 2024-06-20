45370df1bba530252e97b6162aeb6504b7b9af91
Ihor Samartsev <isamarcev1994@gmail.com>
2024-06-20 18:59:00 +0300

    fix image required

--- backend/src/apps/tasks/schemas.py
+++ backend/src/apps/tasks/schemas.py
@@ -46,7 +46,9 @@ class TaskSchema(BaseModel):
     task_id: int
     title: TaskTitle
     description: TaskDescription
-    images: Optional[List[str]] = []
+    images: Optional[List[str]] = Field(
+        ..., description="Image in string format"
+    )
     category: str
     price: PositiveFloat
     currency: str
@@ -97,9 +99,7 @@ class PreCreateTaskSchema(BaseModel):
     title: TaskTitle
     description: TaskDescription
     category: str
-    images: Optional[List[str]] = Field(
-        ..., description="Image in string format"
-    )
+    images: Optional[List[str]] = None
     price: PositiveFloat
     currency: str = Field(..., description="Currency symbol")
     poster_id: PositiveInt = Field(..., description="Customer telegram ID")
